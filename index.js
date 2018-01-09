fe2e78cea20201cf96fa6c0e86301cbdd5e9271e
Gary Morford <morfordg@mst.edu>
2018-01-09 04:04:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
 console.log("Hello, World!")
-nodejs index.js
-console.log("Hello, World!")
-nodejs index.js
-console.log('Hello, World!')
-nodejs index.js
-console.log("Hello, World!")
-// write your code below!nodejs index.js
-nodejs index.js
-console.log("Hello, World!")
